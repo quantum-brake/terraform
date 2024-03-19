10b223a16d47c7fb0b4d387893a2db2a56b0b3e7
quantum_brake <rok9sula@gmail.com>
2024-03-19 20:33:23 +0200

    created :)

---
+++ main.tf
@@ -0,0 +1,28 @@
+locals {
+  tag_name = var.use_locals ? "forum": var.bucket_name
+}
+
+module "s3" {
+  source = "./modules/s3"
+
+  bucket_name = "${data.aws_caller_identity.current.account_id}-my-tf-test-bucket"
+  use_locals = true
+}
+
+module "table_authors" {
+  source = "./modules/dynamodb"
+  context = module.label.context
+  name= "authors"
+}
+
+module "table_courses" {
+  source = "./modules/dynamodb"
+  context = module.label.context
+  name= "courses"
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = module.label_s3.id
+
+  tags = module.label_s3.tags
+}
